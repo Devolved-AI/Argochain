3ab10bef406a7e96d011f0f5d02c80dc48991097
mitun567 <mitun@devolvedai.com>
2024-04-18 16:04:47 +0600

    updated benchmark

--- node/src/benchmarking.rs
+++ node/src/benchmarking.rs
@@ -110,4 +110,13 @@ impl frame_benchmarking_cli::ExtrinsicBuilder for TransferKeepAliveBuilder {
 	}
 }
 
+/// Generates  data inherent for the `benchmark overhead` command.
+pub fn inherent_benchmark_data() -> Result<InherentData> {
+	let mut inherent_data = InherentData::new();
+	let d = Duration::from_millis(0);
+	let timestamp = sp_timestamp::InherentDataProvider::new(d.into());
 
+	futures::executor::block_on(timestamp.provide_inherent_data(&mut inherent_data))
+		.map_err(|e| format!("creating inherent data: {:?}", e))?;
+	Ok(inherent_data)
+}
