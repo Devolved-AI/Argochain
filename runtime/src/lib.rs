baee8550452942931637f78473b3247d75f68020
mitun567 <mitun@devolvedai.com>
2024-06-30 10:48:27 +0600

    updated build

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -628,7 +628,7 @@ impl pallet_session::historical::Config for Runtime {
 parameter_types! {
     pub const SessionsPerEra: sp_staking::SessionIndex = 1;//session 6
     pub const BondingDuration: sp_staking::EraIndex = 24 * 28;
-    pub const SlashDeferDuration: sp_staking::EraIndex = 24 * 7 //1/4 the bonding duration.
+    pub const SlashDeferDuration: sp_staking::EraIndex = 24 * 7; //1/4 the bonding duration.
     // pub const RewardCurve: &'static PiecewiseLinear<'static> = &REWARD_CURVE;
     pub const MaxNominatorRewardedPerValidator: u32 = 256;
     pub const OffendingValidatorsThreshold: Perbill = Perbill::from_percent(17);
