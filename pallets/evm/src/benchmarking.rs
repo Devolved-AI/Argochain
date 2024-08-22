da7e1acbe2f89c57e121555d9bca8465e02bc1a4
Md Raisul Islam Rony <157959679+rony-devolved-AI@users.noreply.github.com>
2024-08-22 22:29:21 +0600

    Create benchmarking.rs
    Signed-off-by: Md Raisul Islam Rony <157959679+rony-devolved-AI@users.noreply.github.com>

---
+++ pallets/evm/src/benchmarking.rs
@@ -0,0 +1,29 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use super::*;
+use frame_benchmarking::{account, benchmarks, impl_benchmark_test_suite};
+use frame_system::RawOrigin;
+use sp_core::H160;
+use sp_runtime::traits::Bounded;
+use sp_runtime::U256;
+
+const SEED: u32 = 0;
+
+benchmarks! {
+    mint_evm_tokens {
+        let caller: T::AccountId = account("caller", 0, SEED);
+        let evm_address: H160 = H160::repeat_byte(1);
+        let amount: U256 = U256::from(1000);
+
+        LastMintingBlock::<T>::put(T::BlockNumber::min_value());
+    }: _(RawOrigin::Root, evm_address, amount)
+    verify {
+        assert!(LastMintingBlock::<T>::get() > T::BlockNumber::min_value());
+    }
+}
+
+impl_benchmark_test_suite!(
+    Pallet,
+    crate::mock::new_test_ext(),
+    crate::mock::Test,
+);
