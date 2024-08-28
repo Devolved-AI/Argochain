13f4c34162887f69048066638b8badd3a2cce9e6
mitun567 <mitun@devolvedai.com>
2024-08-28 10:12:08 +0600

    updated mintprecompiles

--- runtime/src/mint_precompile.rs
+++ runtime/src/mint_precompile.rs
@@ -1,7 +1,5 @@
-use fp_evm::LinearCostPrecompile; 
-
 use fp_evm::{
-    PrecompileFailure, PrecompileHandle, PrecompileOutput, ExitRevert, ExitSucceed,
+    LinearCostPrecompile, PrecompileFailure, PrecompileHandle, ExitRevert, ExitSucceed,
 };
 use sp_core::{H160, U256};
 use sp_runtime::traits::{Saturating, Zero};
@@ -9,6 +7,39 @@ use sp_std::{marker::PhantomData, vec::Vec};
 use frame_support::traits::Currency;
 
 pub struct MintPrecompile<R>(PhantomData<R>);
+
+impl<R> MintPrecompile<R>
+where
+    R: pallet_evm::Config + pallet_balances::Config,
+    R::AccountId: From<H160>,
+    R::Balance: TryFrom<U256> + Zero,
+    <R::Balance as TryFrom<U256>>::Error: core::fmt::Debug,
+{
+    fn parse_input(input: &[u8]) -> Result<(H160, U256), PrecompileFailure> {
+        if input.len() != 52 {
+            return Err(PrecompileFailure::Revert {
+                exit_status: ExitRevert::Reverted,
+                output: b"Invalid input length".to_vec(),
+            });
+        }
+
+        let evm_address = H160::from_slice(&input[0..20]);
+        let amount_to_mint = U256::from_big_endian(&input[20..52]);
+
+        Ok((evm_address, amount_to_mint))
+    }
+
+    fn convert_to_balance(amount: U256) -> Result<R::Balance, PrecompileFailure> {
+        amount.try_into().map_err(|_| PrecompileFailure::Revert {
+            exit_status: ExitRevert::Reverted,
+            output: b"Failed to convert U256 to Balance".to_vec(),
+        })
+    }
+
+    fn mint_to_account(account_id: &R::AccountId, amount: R::Balance) {
+        R::Currency::deposit_creating(account_id, amount);
+    }
+}
 
 impl<R> LinearCostPrecompile for MintPrecompile<R>
 where
@@ -25,28 +56,13 @@ where
     ) -> Result<(ExitSucceed, Vec<u8>), PrecompileFailure> {
         let input = handle.input();
 
-        if input.len() != 52 {
-            return Err(PrecompileFailure::Revert {
-                exit_status: ExitRevert::Reverted,
-                output: b"Invalid input length".to_vec(),
-            });
-        }
-
-        let evm_address = H160::from_slice(&input[0..20]);
-        let amount_to_mint = U256::from_big_endian(&input[20..52]);
+        let (evm_address, amount_to_mint) = Self::parse_input(input)?;
 
         let account_id: R::AccountId = evm_address.into();
 
-        let amount_to_mint_balance: R::Balance = amount_to_mint.try_into().map_err(|_| PrecompileFailure::Revert {
-            exit_status: ExitRevert::Reverted,
-            output: b"Failed to convert U256 to Balance".to_vec(),
-        })?;
+        let amount_to_mint_balance = Self::convert_to_balance(amount_to_mint)?;
 
-        let current_balance: R::Balance = R::Currency::free_balance(&account_id);
-
-        let new_balance = current_balance.saturating_add(amount_to_mint_balance);
-
-        R::Currency::deposit_creating(&account_id, new_balance);
+        Self::mint_to_account(&account_id, amount_to_mint_balance);
 
         Ok((ExitSucceed::Returned, Vec::new()))
     }
