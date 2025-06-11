028f99eab2dea87461bf2fa7eeae0519bc3db429
mitun567 <mitun@devolvedai.com>
2025-06-11 17:03:51 +0600

    changes with benchmark

--- frame/pallet-counter/src/benchmarking.rs
+++ frame/pallet-counter/src/benchmarking.rs
@@ -1,53 +1,52 @@
 #[cfg(feature = "runtime-benchmarks")]
-mod benchmarking;
+mod benchmarking {
+    use super::*;
+    use frame_benchmarking::{benchmarks, whitelisted_caller};
+    use frame_system::RawOrigin;
 
+    benchmarks! {
+        mint {
+            let caller: T::AccountId = whitelisted_caller();
+            let amount: SubstrateBalanceOf<T> = 100u32.into();
+        }: _(RawOrigin::Root, caller.clone(), amount)
+        verify {
+            assert_eq!(T::SubstrateCurrency::free_balance(&caller), amount);
+        }
 
-use super::*;
-use frame_benchmarking::{benchmarks, whitelisted_caller, account};
-use frame_system::RawOrigin;
+        burn {
+            let caller: T::AccountId = whitelisted_caller();
+            let amount: SubstrateBalanceOf<T> = 100u32.into();
+            T::SubstrateCurrency::deposit_creating(&caller, amount);
+        }: _(RawOrigin::Root, caller.clone(), amount)
+        verify {
+            assert_eq!(T::SubstrateCurrency::free_balance(&caller), 0u32.into());
+        }
 
-benchmarks! {
-    mint {
-        let caller: T::AccountId = whitelisted_caller();
-        let amount: BalanceOf<T> = 100u32.into();
-    }: _(RawOrigin::Root, caller.clone(), amount)
-    verify {
-        assert_eq!(T::Currency::free_balance(&caller), amount);
+        lock {
+            let caller: T::AccountId = whitelisted_caller();
+            let amount: SubstrateBalanceOf<T> = 100u32.into();
+            T::SubstrateCurrency::deposit_creating(&caller, amount);
+        }: _(RawOrigin::Signed(caller.clone()), amount)
+        verify {
+            assert_eq!(T::SubstrateCurrency::reserved_balance(&caller), amount);
+            assert_eq!(LockedBalance::<T>::get(&caller), amount);
+        }
+
+        unlock {
+            let caller: T::AccountId = whitelisted_caller();
+            let amount: SubstrateBalanceOf<T> = 100u32.into();
+            T::SubstrateCurrency::reserve(&caller, amount)?;
+            <LockedBalance<T>>::insert(&caller, amount);
+        }: _(RawOrigin::Signed(caller.clone()), amount)
+        verify {
+            assert_eq!(T::SubstrateCurrency::reserved_balance(&caller), 0u32.into());
+            assert_eq!(LockedBalance::<T>::get(&caller), 0u32.into());
+        }
     }
 
-    burn {
-        let caller: T::AccountId = whitelisted_caller();
-        let amount: BalanceOf<T> = 100u32.into();
-        T::Currency::deposit_creating(&caller, amount);
-    }: _(RawOrigin::Root, caller.clone(), amount)
-    verify {
-        assert_eq!(T::Currency::free_balance(&caller), 0u32.into());
-    }
-
-    lock {
-        let caller: T::AccountId = whitelisted_caller();
-        let amount: BalanceOf<T> = 100u32.into();
-        T::Currency::deposit_creating(&caller, amount);
-    }: _(RawOrigin::Signed(caller.clone()), amount)
-    verify {
-        assert_eq!(T::Currency::reserved_balance(&caller), amount);
-        assert_eq!(LockedBalance::<T>::get(&caller), amount);
-    }
-
-    unlock {
-        let caller: T::AccountId = whitelisted_caller();
-        let amount: BalanceOf<T> = 100u32.into();
-        T::Currency::reserve(&caller, amount)?;
-        <LockedBalance<T>>::insert(&caller, amount);
-    }: _(RawOrigin::Signed(caller.clone()), amount)
-    verify {
-        assert_eq!(T::Currency::reserved_balance(&caller), 0u32.into());
-        assert_eq!(LockedBalance::<T>::get(&caller), 0u32.into());
-    }
+    impl_benchmark_test_suite!(
+        Pallet,
+        crate::mock::new_test_ext(),
+        crate::mock::Test,
+    );
 }
-
-impl_benchmark_test_suite!(
-    Pallet,
-    crate::mock::new_test_ext(),
-    crate::mock::Test,
-);
